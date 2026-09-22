6d006a6a7a31df71539888f55120df4df4ad899d
a <arr.novozhilov@gmail.com>
2026-09-22 20:38:56 +0300

    fix character instance attributes

--- character.py
+++ character.py
@@ -1,10 +1,5 @@
 class Character:
-    maxhealth = 100
-    max_mana = 100
-    gold = 0
-    level = 1
-    experience = 0
-    def __init__(self, name, character_class, health, mana, strength, defense):
+    def __init__(self, name, character_class, health, mana, strength, defense, experience, gold, level, max_health, max_mana):
         self.name = name
         self.character_class = character_class
 
@@ -13,3 +8,28 @@ class Character:
 
         self.strength = strength
         self.defense = defense
+
+        self.max_health = max_health
+        self.max_mana = max_mana
+        self.gold = gold
+        self.level = level
+        self.experience = experience
+    def __str__(self):
+        print('== == == == == == == == == ==')
+        print('CHARACTER')
+
+        print(f'Name: ', self.name)
+        print(f'Class: ', self.character_class)
+        print(f'Level: ', self.level)
+        print(f'Exp: ', self.experience)
+
+        print(f'HP: {self.health} / {self.max_health}')
+        print(f'Mana: {self.mana} / {self.max_mana}')
+
+        print(f'Strength: ', self.strength)
+        print(f'Defense: ', self.defense)
+
+        print(f'Gold: ', self.gold)
+        print('== == == == == == == == == ==')
+
+
