cefe5a81f885ddee635afd5af1392dd09a5e59ac
a <arr.novozhilov@gmail.com>
2026-09-22 19:58:24 +0300

    initial project and main menu

---
+++ main.py
@@ -0,0 +1,22 @@
+def menu():
+    print("======================")
+    print("      TEXT RPG        ")
+    print("======================")
+
+    print("1. New Game")
+    print("2. Load Game")
+    print("3. Exit")
+
+def choice_menu_option():
+    option = input()
+    if option == "1": print("This don't work yet:(")
+    elif option == "2": print("This don't work yet:(")
+    elif option == "3": print("Goodbye")
+    else: print("Please enter a valid option")
+
+
+while True:
+    menu()
+    choice_menu_option()
+
+
