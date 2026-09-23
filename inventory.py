c4c2a7d82946b3ae661d1548ecbeeb1f0022c2f5
a <arr.novozhilov@gmail.com>
2026-09-23 14:11:24 +0300

    добавлены зельки

--- inventory.py
+++ inventory.py
@@ -3,6 +3,26 @@ class Inventory:
         self.items = []
         self.max_capacity = max_capacity
 
+
+    def use_item(self, item, character):
+        if item in self.items:
+            if item.item_type == "Potion":
+                if character.health < character.max_health:
+                    health_before = character.health
+                    character.heal(item.effect_value)
+                    health_after = character.health
+                    amount_healed = health_after - health_before
+                    print(f"Братан ты вылечился на {amount_healed} HP")
+                    self.remove_item(item)
+                else:
+                    print("Пацан ты здоров как бык!")
+            else:
+                print("Братан ты не можешь это попить!")
+        else:
+            print("Братан у тебя этой шняги нету!")
+
+
+
     def add_item(self, item):
         if len(self.items) < self.max_capacity:
             self.items.append(item)
--- item.py
+++ item.py
@@ -1,15 +1,19 @@
 class Item:
-    def __init__(self, name, item_type, value):
+    def __init__(self, name, item_type, value, effect_value=None):
         self.name = name
         self.item_type = item_type
         self.value = value
+        self.effect_value = effect_value
 
     def describe_item(self):
         print('         ')
         print('Предмет: ', self.name)
         print('Тип: ', self.item_type)
         print(f'Стоимость: {self.value} золота')
+        print('Эффект: ', self.effect_value)
         print('           ')
 
 
 
+temp = Item('Палка', 'Weapon', 20)
+temp.describe_item()
