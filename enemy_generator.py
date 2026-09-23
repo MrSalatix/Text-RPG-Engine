1dec12722641db0a2f8764fc398dd60ed0175f14
a <arr.novozhilov@gmail.com>
2026-09-23 11:28:10 +0300

    добавлен генератор противниов

---
+++ enemy_generator.py
@@ -0,0 +1,16 @@
+import random
+from enemy import Enemy
+
+enemies = {
+    1: {'name': 'Skeleton', 'strength': 5, 'defense': 0, 'max_health' : 50, 'experience_reward': 10, 'gold_reward': 5},
+    2: {'name': 'Goblin', 'strength': 10, 'defense': 5, 'max_health': 100, 'experience_reward': 25, 'gold_reward': 15},
+    3: {'name': 'Orc', 'strength': 20, 'defense': 20, 'max_health': 200, 'experience_reward': 50, 'gold_reward': 30}
+}
+
+def generate_enemy():
+    raw_enemy = random.choice(list(enemies.values()))
+
+    generated_enemy = Enemy(raw_enemy['name'], raw_enemy['strength'], raw_enemy['defense'], raw_enemy['max_health'], raw_enemy['experience_reward'], raw_enemy['gold_reward'])
+
+    return generated_enemy
+
